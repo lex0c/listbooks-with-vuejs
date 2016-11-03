11698d806537a874cc3632efb5096d68bb3fe165
Léo Castro <leonardo_carvalho@outlook.com>
2016-11-03 21:52:42 -0200

    Implementing filters...

--- core/app.js
+++ core/app.js
@@ -2,11 +2,23 @@ var app = new Vue({
     el: "#app",
     
     data: {
-        books: []
+        books: [],
+        search: ""
     },
 
     methods: {
         
+    },
+    filters: {
+        uppercase: function(value){
+            if(!value) return ''
+            value = value.toString()
+            return value.toUpperCase();
+       },
+       filterBy: function(data){
+       	    
+       }
+
     },
     
     mounted: function(){
